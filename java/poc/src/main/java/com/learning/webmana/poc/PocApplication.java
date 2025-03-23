be5f752fc012d16b74f605fb8b50e4aa7b29a46a
Nguyễn Lê Phong <nguyenlephong1997@gmail.com>
2025-03-23 11:27:02 +0700

    feat: integration swagger ui

--- java/poc/src/main/java/com/learning/webmana/poc/PocApplication.java
+++ java/poc/src/main/java/com/learning/webmana/poc/PocApplication.java
@@ -1,8 +1,13 @@
 package com.learning.webmana.poc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+    info = @Info(title = "Learning App", version = "1.0", description = "API PoC Learning App")
+)
 @SpringBootApplication
 public class PocApplication {
 
--- java/poc/src/main/java/com/learning/webmana/poc/controller/VocabularyController.java
+++ java/poc/src/main/java/com/learning/webmana/poc/controller/VocabularyController.java
@@ -4,11 +4,14 @@ import com.learning.webmana.poc.model.Vocabulary;
 import com.learning.webmana.poc.service.VocabularyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/vocabulary")
+@Tag(name = "Vocabulary API", description = "CRUD operations for vocabulary words")
 public class VocabularyController {
     private final VocabularyService vocabularyService;
 
@@ -17,22 +20,26 @@ public class VocabularyController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all words", description = "Retrieves a list of vocabulary words")
     public List<Vocabulary> getAllWords() {
         return vocabularyService.getAllWords();
     }
 
     @PostMapping
+    @Operation(summary = "Add a new word", description = "Adds a new vocabulary word")
     public ResponseEntity<Vocabulary> addWord(@RequestBody Vocabulary vocabulary) {
         return ResponseEntity.ok(vocabularyService.addWord(vocabulary));
     }
 
     @PutMapping("/{word}")
+    @Operation(summary = "Update a word", description = "Updates the meaning of an existing vocabulary word")
     public ResponseEntity<Vocabulary> updateWord(@PathVariable String word, @RequestBody Vocabulary updatedVocabulary) {
         Vocabulary updated = vocabularyService.updateWord(word, updatedVocabulary);
         return (updated != null) ? ResponseEntity.ok(updated) : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{word}")
+    @Operation(summary = "Delete a word", description = "Deletes a vocabulary word")
     public ResponseEntity<String> deleteWord(@PathVariable String word) {
         return vocabularyService.deleteWord(word)
                 ? ResponseEntity.ok("Deleted successfully")
