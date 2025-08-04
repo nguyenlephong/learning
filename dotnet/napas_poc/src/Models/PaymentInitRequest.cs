9f75a47f3b8ff2fc45e7d374ead5013b24c27c0f
Phong Nguyen <phongnl@vng.com.vn>
2025-08-05 05:42:48 +0700

    [.net] delete(poc): clean up unused source

--- dotnet/napas_poc/src/Models/PaymentInitRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace napas_poc.Models
-{
-    public class PaymentInitRequest
-    {
-        public string OrderId { get; set; } = string.Empty;
-        public decimal Amount { get; set; }
-        public string Description { get; set; } = string.Empty;
-    }
-}
--- dotnet/napas_poc/src/Models/PaymentInitResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace napas_poc.Models
-{
-    public class PaymentInitResponse
-    {
-        public string PaymentUrl { get; set; } = string.Empty;
-        public string OrderId { get; set; } = string.Empty;
-    }
-}
